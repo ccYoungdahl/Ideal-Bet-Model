598625378bc6f1ceca746617708e23f491242b45
Caleb Youngdahl <112095056+ccYoungdahl@users.noreply.github.com>
2025-04-24 12:05:44 -0400

    Update features.py

--- features.py
+++ features.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
-
-
-common_features = [
-    'home_avg_pts_last_3', 'home_avg_pts_last_5', 'home_rest_days', 'home_win_streak_last_10',
-    'away_avg_pts_last_3', 'away_avg_pts_last_5', 'away_rest_days', 'away_win_streak_last_10',
-    'form_diff_pts_3', 'rest_advantage', 'win_streak_diff',
-    'fg_pct_diff_form', 'reb_diff_form', 'tov_diff_form',
-    'is_home_back_to_back', 'is_away_back_to_back',
-    'implied_prob_home', 'implied_prob_away',
-    'spread_point', 'outcome_point_Over'
+COMMON_BASE = [
+    "home_win_pct_last10","away_win_pct_last10","win_pct_diff_last10",
+    "rest_advantage","win_streak_diff","home_rest_days","away_rest_days","is_home_back_to_back",
+    "is_away_back_to_back","fg_pct_roll3_home","fg_pct_roll3_away","home_fg3a_last5","away_fg3a_last5","fg3a_diff_last5",
+    "home_fta_last5","away_fta_last5","fta_diff_last5",
 ]
+
+MONEYLINE_FEATURES = COMMON_BASE + [
+    "home_offrtg_last5","away_offrtg_last5","offrtg_diff_last5",
+    "implied_prob_home","implied_prob_away","tov_diff_form","home_win_pct_season","away_win_pct_season",
+    "win_pct_diff_season"
+]
+
+SPREAD_FEATURES = COMMON_BASE + [
+    "pace_diff_last5","netrtg_diff_last5","spread_point",
+    "efg_pct_diff_last5","fga_diff_last5","tov_diff_form","implied_prob_home","implied_prob_away","home_win_pct_season","away_win_pct_season",
+    "win_pct_diff_season"
+]
+
+OVERUNDER_FEATURES = [
+    "pace_adj_total","home_pace_last5","away_pace_last5","pace_diff_last5",
+    "home_ppg_last5","away_ppg_last5","ppg_diff_last5",
+    "offrtg_diff_last5","defrtg_diff_last5","netrtg_diff_last5","outcome_point_Over",
+]
+
+
